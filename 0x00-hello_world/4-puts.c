b677171690776c0bf3a32ad2f01eacbe105d2cad
Mohamed E. R <m.rahmani-96@live.com>
2022-09-08 09:04:18 -0700

    Fixed 0x00/Task 4: puts.c script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main entry point
+ *
+ * Return: Always 0 (no errors)
+ */
+
+int main(void)
+{
+  char MyInput[] = "Programming is like building a multilingual puzzle\n";
+
+  puts(MyInput);
+
+  return(0);
+
+}
