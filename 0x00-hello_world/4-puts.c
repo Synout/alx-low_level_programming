a2b76f34949449d2a383d4a71d87376e1fb46a72
Mohamed E. R <m.rahmani-96@live.com>
2022-09-08 16:16:22 -0700

    Attempted fix on 0x00/Task 4: puts.c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * Description:  prints a predefined text using puts
+ * main - entry point function
+ * Description -  prints a predefined text using puts
  * Return: Always 0 (no errors)
  */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
