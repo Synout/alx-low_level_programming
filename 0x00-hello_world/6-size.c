dead9b15bac17a9a905736e4157e71e593470ca8
Mohamed E. R <m.rahmani-96@live.com>
2022-09-08 17:46:16 -0700

    Fixed 0x00/Task 6: size.c script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int b;
 long int c;
 long long int d;
 float e;
-printf("Size of char: %ld byte(s)\n", sizeof(a));
-printf("Size of int: %ld byte(s)\n", sizeof(b));
-printf("Size of long int: %ld byte(s)\n", sizeof(c));
-printf("Size of long long int: %ld byte(s)\n", sizeof(d));
-printf("Size of float: %ld byte(s)\n", sizeof(e));
+printf("Size of a char: %ld byte(s)\n", sizeof(a));
+printf("Size of an int: %ld byte(s)\n", sizeof(b));
+printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
+printf("Size of a float: %ld byte(s)\n", sizeof(e));
 return (0);
 }
