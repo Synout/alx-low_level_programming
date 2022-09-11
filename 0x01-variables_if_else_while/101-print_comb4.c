782edeaf35fd49ed55d454f72fe42f781998c65c
Mohamed E. R <m.rahmani-96@live.com>
2022-09-10 17:40:31 -0700

    Added 0x01/Task 11: print_comb4.c script

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+#include <stdio.h>
+
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point function
+ * Description - prints combination of three digit numbers
+ * Return: 0 (no errors)
+ */
+
+int main(void)
+{
+/* your code goes there */
+int i = 0;
+int j = 0;
+int k = 0;
+for (; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+putchar (i % 10 + '0');
+putchar (j % 10 + '0');
+putchar (k % 10 + '0');
+if (i != 7)
+{
+putchar (',');
+putchar (' ');
+}
+}
+}
+}
+putchar ('\n');
+return (0);
+}
