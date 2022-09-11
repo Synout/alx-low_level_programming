442b9b46532f5aba033ffe8e2379393244ff3d9c
Mohamed E. R <m.rahmani-96@live.com>
2022-09-10 17:30:05 -0700

    Fixed 0x01/Task 10: print_comb3.c script

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,13 +19,13 @@ int main(void)
 /* your code goes there */
 int i = 0;
 int j = 0;
-char MyArray[10] = "0123456789";
+
 for (; i < 10; i++)
 {
 for (j = i + 1; j < 10; j++)
 {
-putchar (MyArray[i]);
-putchar (MyArray[j]);
+putchar (i%10 + '0');
+putchar (j%10 + '0');
 if (i != 8)
 {
 putchar (',');
