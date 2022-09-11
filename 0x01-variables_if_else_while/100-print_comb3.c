505a13373601baf3e271efee22263bdf9257b2e1
Mohamed E. R <m.rahmani-96@live.com>
2022-09-10 17:31:18 -0700

    Fixed 0x01/Task 10: print_comb3.c script

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -24,8 +24,8 @@ for (; i < 10; i++)
 {
 for (j = i + 1; j < 10; j++)
 {
-putchar (i%10 + '0');
-putchar (j%10 + '0');
+putchar (i % 10 + '0');
+putchar (j % 10 + '0');
 if (i != 8)
 {
 putchar (',');
