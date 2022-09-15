1e99c19aa34814b20eaa69376c565fe4338ed1a4
Mohamed E. R <m.rahmani-96@live.com>
2022-09-15 13:08:08 -0700

    fixed 0x03/Task 1: 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,8 @@
 
 /**
 * main - causes an infinite loop
+* Description: Avoids infinite loop by commenting
+* @\\o/\n": Metal hand
 * Return: 0
 */
 
@@ -13,11 +15,12 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-//while (i < 10)
-//{
-//putchar(i);      
-//}
-
+/**
+*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
