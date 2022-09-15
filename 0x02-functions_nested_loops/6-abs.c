0c4c91d0490f3c5a5136c9f610f2593f408651ae
Mohamed E. R <m.rahmani-96@live.com>
2022-09-14 18:38:50 -0700

    Added 0x02/Task 6: abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - function to print absolute of value.
+ * @c: input character
+ *
+ * Return: int.
+ */
+int _abs(int c)
+{
+if (c < 0)
+{
+c *= -1;
+return (c);
+}
+else
+{
+return (c);
+}
+}
