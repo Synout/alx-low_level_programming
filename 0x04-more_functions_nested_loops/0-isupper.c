09e8dcd7915504934d9db006603d833113f644da
Mohamed E. R <m.rahmani-96@live.com>
2022-09-15 18:20:18 -0700

    Added 0x04/Task 0: isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _isupper - check ig char is upper case
+* @c: character to check
+* Return: 0 if lower or 1 upper
+*/
+
+int _isupper(int c)
+{
+return (c >= 65 && c <= 90);
+}
