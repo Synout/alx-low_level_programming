0372c32a5dc69f45773ea9be19beb69dbdae0434
Mohamed E. R <m.rahmani-96@live.com>
2022-09-16 07:56:37 -0700

    Added 0x04/Task 4: print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints int from 0 to 9 except 2, 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+int i = 0;
+
+for (; i <= 9; i++)
+{
+if (i != 2 && i != 4)
+{
+_putchar(i + '0');
+}
+}
+_putchar('\n');
+}
