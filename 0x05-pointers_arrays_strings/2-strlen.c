c36dcf635023d3e3d07e5adc06aab89c2b03474e
Mohamed E. R <m.rahmani-96@live.com>
2022-09-19 07:31:41 -0700

    Added 0x05/Task 2: strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _strlen - function that returns the length of strength
+* @s: string to get length of
+* Return: int
+*/
+
+int _strlen(char *s)
+{
+int i = 0;
+for (;s[i]; i++);
+return i;
+}
