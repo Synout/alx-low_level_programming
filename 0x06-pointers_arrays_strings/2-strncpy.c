e0e519706543a46ba3760982e9db4d881b9a41fb
Mohamed E. R <m.rahmani-96@live.com>
2022-09-21 16:42:20 -0700

    Added 0x06/Task 2: strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* *_strncpy - function that concats two strinfes
+* @src: string to copy from
+* @dest: string to copy to
+* @n: the number of bytes to use  from src
+* Return: char
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+int i = 0;
+int k = 0;
+
+for (; dest[i];)
+{
+i++;
+}
+
+for (; src[k] && k < n; k++)
+{
+dest[k] = src[k];
+}
+
+return (dest);
+}
