2ba1f348d1faf8cabd0882fe06c880932793a9ba
Mohamed E. R <m.rahmani-96@live.com>
2022-09-23 06:52:04 -0700

    Added 0x06/Task 6: cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* cap_string - function that converts string to capitalized
+* @s: string to make capitalized
+* Return: void
+*/
+
+char *cap_string(char *s)
+{
+int i = 0;
+
+for (; s[i]; i++)
+{
+if (s[i] <= 'z' && s[i] >= 'a')
+{
+if (!(s[i-1] <= 'z' && s[i-1] >= 'a') && !(s[i-1] <= 'Z' && s[i-1] >= 'A'))
+{
+s[i] = toupper(s[i]);
+}
+}
+}
+return (s);
+}
