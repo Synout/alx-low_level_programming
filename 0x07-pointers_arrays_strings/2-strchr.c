4c29ec73cce14e0ba6f6a688c6fa1225eff1d948
Mohamed E. R <m.rahmani-96@live.com>
2022-09-26 15:25:07 -0700

    Added 0x07/Task 2: strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strchr - function that looks for first occurence of char or NULL
+* @s: string to search in
+* @c: character to look for
+* Return: char or NULL
+*/
+
+char *_strchr(char *s, char c)
+{
+
+int i = 0;
+
+for (; s[i]; i++)
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+}
+return (NULL);
+}
