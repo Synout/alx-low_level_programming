5c1ced6274ca9570a09a52f45be3bd6e5abfedf6
Mohamed E. R <m.rahmani-96@live.com>
2022-09-26 03:43:27 -0700

    Added 0x07/Task 1: memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* *memcpy(char b, unsigned int n); - function that copies mem with bytes
+* @dest: pointer to copy to
+* @src: bytes to copy from
+* @n : number of bytes to replace
+* Return: char
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+unsigned int i = 0;
+
+
+for (; i < n; i++)
+{
+dest[i] = src[i];
+}
+
+return (dest);
+}
