4711109b74c1c9e5d85b304a4828b20bfa8d6d67
Mohamed E. R <m.rahmani-96@live.com>
2022-09-25 22:39:14 -0700

    Added 0x07/Task 0: memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* *memset(char b, unsigned int n); - function that fills mem with bytes
+* @s: pointer t area
+* @b: bytes to replace
+* @n : number of bytes to replace
+* Return: char
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+unsigned int i = 0;
+
+
+for (; i < n; i++)
+{
+s[i] = b;
+}
+
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <ctype.h>
+
+char *_memset(char *s, char b, unsigned int n);
+
+char _putchar(char c);
+
+#endif /* MAIN_H */
