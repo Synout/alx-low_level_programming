2eb4362069f80790afcf03996024c3a0b9354156
Mohamed E. R <m.rahmani-96@live.com>
2022-09-30 16:10:35 -0700

    Added 0x09/Task 0: libmy.a and main.h

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _atoi - empty prototype func
+ * @s: The character input
+ *
+ * Return: int
+ * 
+ */
+int _atoi(char *s)
+{
+return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+This is my Static Libraries in C tasks folder
