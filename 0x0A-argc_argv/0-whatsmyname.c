ba1526148bee5eb2a9df63e0ebd278773c24f804
Mohamed E. R <m.rahmani-96@live.com>
2022-09-30 16:43:01 -0700

    Added 0x0A/Task 0: whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point function
+ * @argc: the array size of inputs
+ * @argv: array of inputs
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - 1]);
+return (0);
+}
