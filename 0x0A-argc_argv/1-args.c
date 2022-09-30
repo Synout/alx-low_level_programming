2a9a1054dd4b2356c771ffe769cd4daf44a36d89
Mohamed E. R <m.rahmani-96@live.com>
2022-09-30 16:47:22 -0700

    Added 0x0A/Task 1: args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point function
+ * @argc: the array size of inputs
+ * @argv: array of inputs
+ * Return: always 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
