6f4087e3801da7cccff8c77ceec11a8a66634922
Mohamed E. R <m.rahmani-96@live.com>
2022-10-04 16:42:12 -0700

    Added 0x0B/Task 2: str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * str_concat - returns pointer to mem with cp of string
+ *
+ * @s1 : string to concat
+ * @s2 : string to concat
+ * Return: NULL if str = NULL, else char
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+unsigned int i = 0;
+unsigned int j = 0;
+unsigned int k;
+char *array;
+
+if (s1 == NULL && s2 == NULL)
+{
+return (NULL);
+}
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+
+if (s2 == NULL)
+{
+s2 = "";
+}
+
+for (; s1[i];)
+{
+i++;
+}
+
+for (; s2[j];)
+{
+j++;
+}
+
+k = i + j;
+array = malloc(sizeof(char) * (k + 1));
+
+if (array == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; s1[i]; i++)
+{
+array[i] = s1[i];
+}
+
+for (j = 0; s2[j]; j++)
+{
+array[i] = s2[j];
+i++;
+}
+i++;
+array[i + j] = '\0';
+return (array);
+}
