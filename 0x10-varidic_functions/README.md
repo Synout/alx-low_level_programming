276f9add418fb760adc0e533895571c5d9149dce
Mohamed E. R <m.rahmani-96@live.com>
2022-10-13 01:32:28 -0700

    Added 0x10/Task 0: sum_them_all.c

---
+++ 0x10-varidic_functions/README.md
@@ -0,0 +1,1 @@
+This is my Varidic Functions in C tasks folder.
---
+++ 0x10-varidic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_H
+#define VARIADIC_H
+
+#include <stdarg.h>
+
+
+char _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARIADIC_H */
