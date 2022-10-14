67bbf1e0c888e788556a2c6c5f9edd06a37f1ae6
Mohamed E. R <m.rahmani-96@live.com>
2022-10-13 19:57:16 -0700

    Added 0x10/Task 1: print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,10 +18,10 @@ va_list nums;
 unsigned int i = 0;
 
 va_start(nums, n);
-for(; i < n; i++)
+for (; i < n; i++)
 {
 printf("%d", va_arg(nums, int));
-if ( i != (n - 1) && separator != NULL)
+if (i != (n - 1) && separator != NULL)
 {
 printf("%s", separator);
 }
