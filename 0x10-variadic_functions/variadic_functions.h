ea42be5bd60abb7e43cf6f5b229f828015529e28
Mohamed E. R <m.rahmani-96@live.com>
2022-10-13 20:08:52 -0700

    Added 0x10/Task 2: print_strings.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,12 @@
 
 #include <stdarg.h>
 
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
 
 char _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
